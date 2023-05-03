bd30bd54eb3cb02aa38c5562c5768d26221460e7
Matheus Guimarães <mgguima95@gmail.com>
2023-05-02 22:42:17 -0300

    Catching error

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const nameInput = document.querySelector('.name-input');
 const telInput = document.querySelector('.tel-input');
 
 
-button.addEventListener('click', () => {      
+button.addEventListener('click', (e) => {      
+    e.preventDefault();
     const donor = {
         name : nameInput.value,
         telefone : telInput.value
@@ -12,9 +13,9 @@ button.addEventListener('click', () => {
     fetch('https://us-central1-projeto-vida-ec162.cloudfunctions.net/api/donors', {
         method: "POST",
         body: JSON.stringify(donor),
-        headers: {"Content-type": "application/json; charset=UTF-8"
-        }
+        headers: {"Content-type": "application/json; charset=UTF-8"}
         })
         .then(response => response.json()) 
-        .then(json => console.log(json));
+        .then(json => console.log(json))
+        .catch(error => console.log(error))
 })
