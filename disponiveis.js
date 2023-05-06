8a6843c176bbdc612e6287aabc2121cd1751a77a
Matheus Guimarães <mgguima95@gmail.com>
2023-05-06 18:39:15 -0300

    Adjustments to retrieve donors

--- disponiveis.js
+++ disponiveis.js
@@ -1,7 +1,10 @@
 const list = document.querySelector('.donor-list');
 
 fetch('https://us-central1-projeto-vida-ec162.cloudfunctions.net/api/donors').
-    then(response => createDonors(response.body));
+    then(response => {
+        console.log(response.body)
+        createDonors(response.body)
+    });
 
 
 function createDonors(donors) {
