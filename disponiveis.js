015d9e632899cc1fd597b9892dcbc0bd36ef8470
Matheus Guimarães <mgguima95@gmail.com>
2023-05-06 18:25:09 -0300

    Adjusting donor list in correct page

--- disponiveis.js
+++ disponiveis.js
@@ -1,4 +1,15 @@
 const list = document.querySelector('.donor-list');
 
 fetch('https://us-central1-projeto-vida-ec162.cloudfunctions.net/api/donors').
-    then(response => console.log(response))
+    then(response => createDonors(response.body));
+
+
+function createDonors(donors) {
+    for(let i = 0; i < donors.length; i++) {
+        const newDonor = document.createElement('li');
+        const donorName = document.createElement('p');
+        donorName.innerHTML = donors[i].donorName;
+        element.appendChild(donorName);
+        list.appendChild(newDonor);
+    }
+}
