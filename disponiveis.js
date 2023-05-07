b38aaab884fab714b08d51e73a161b7f773fb917
Matheus Guimarães <mgguima95@gmail.com>
2023-05-06 21:59:48 -0300

    Debugging

--- disponiveis.js
+++ disponiveis.js
@@ -3,6 +3,7 @@ const list = document.querySelector('.donor-list');
 fetch('https://us-central1-projeto-vida-ec162.cloudfunctions.net/api/donors').
     then(response => {
         response = response.json();
+        console.log(response);
         createDonors(response)
     });
 
