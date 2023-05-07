ff374c56b9a4ddcb469a32fd62094d8a5cb4303e
Matheus Guimarães <mgguima95@gmail.com>
2023-05-06 21:15:35 -0300

    Refactoring to match ReadableStream

--- disponiveis.js
+++ disponiveis.js
@@ -1,14 +1,11 @@
 const list = document.querySelector('.donor-list');
 
 fetch('https://us-central1-projeto-vida-ec162.cloudfunctions.net/api/donors').
-    then(response => {
-        console.log(response.body)
-        createDonors(response.body)
-    });
+    then(response => {createDonors(response.body)});
 
 
 function createDonors(donors) {
-    for(let i = 0; i < donors.length; i++) {
+    for await (const donor of donors) {
         const newDonor = document.createElement('li');
         const donorName = document.createElement('p');
         donorName.innerHTML = donors[i].donorName;
